3d33a19b495660d9cb0bc01154eb3a6d113e77ad
Tselena <etsoy@inno.tech>
2025-10-24 16:03:12 +1000

    changed program logic - arithmetic operations

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+
+        int sum = number1 + number2;
+        int subtraction = number1 - number2;
+        int multiplication = number1 * number2;
+        double division = (double) number1 / number2;
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + subtraction);
+        System.out.println("Произведение: " + multiplication);
+        System.out.println("Частное: " + division);
     }
 }
