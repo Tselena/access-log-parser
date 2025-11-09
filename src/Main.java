889dbe1cce7733e111b395c6db210559424c2bcf
Tselena <etsoy@inno.tech>
2025-11-09 19:23:21 +1000

    correct logic mistake in code

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,14 @@ public class Main {
     public static void main(String[] args) {
         int file_count = 0;
         while(true) {
+            System.out.println("Укажите путь к файлу");
             String path = new Scanner(System.in).nextLine(); //запрашиваем путь к файлу
 
             File file = new File(path);
             boolean fileExists = file.exists(); //проверка, существует ли указнный файл
             boolean isDirectory = file.isDirectory(); //проверка, что путь ведет к файлу
 
-            if (!fileExists && isDirectory) {
+            if (!fileExists || isDirectory) {
                 System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не к файлу");
                 continue;
             }
